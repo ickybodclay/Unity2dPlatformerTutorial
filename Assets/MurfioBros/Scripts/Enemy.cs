367f49eb8e4c1b971a964700e59e0b532a40619c
Jason Petterson <jp@brokenshotgun.com>
2017-03-03 22:29:48 -0500

    Episode 1 - Creating a basic level and goomba-like enemy

---
+++ Assets/MurfioBros/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    Collider2D[] m_colliders;
+
+    private void Start() {
+        m_colliders = GetComponentsInChildren<Collider2D>();
+    }
+
+    public void Kill() {
+        Invoke("DisableColliders", .25f);
+        Destroy(gameObject, 5f);
+    }
+
+    private void DisableColliders() {
+        foreach (Collider2D col in m_colliders) {
+            col.enabled = false;
+        }
+    }
+}
---
+++ Assets/MurfioBros/Scripts/EnemyHead.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHead : MonoBehaviour {
+
+    [SerializeField] Enemy m_enemy;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.transform.tag == "Player") {
+            m_enemy.Kill();
+        }
+    }
+}
