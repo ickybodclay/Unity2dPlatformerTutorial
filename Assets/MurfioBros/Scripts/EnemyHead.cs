f17fe6bdd14504aa8f0fce4548b657cf8e638fcc
Jason Petterson <jp@brokenshotgun.com>
2017-03-06 22:14:35 -0500

    Episode 2 - Fix enemy head collision detection and add Player health

--- Assets/MurfioBros/Scripts/EnemyHead.cs
+++ Assets/MurfioBros/Scripts/EnemyHead.cs
@@ -7,7 +7,9 @@ public class EnemyHead : MonoBehaviour {
     [SerializeField] Enemy m_enemy;
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.transform.tag == "Player") {
+        if (collision.transform.tag == "Player" && 
+            collision.collider is CircleCollider2D && 
+            collision.contacts[0].otherCollider.tag == "Head") {
             m_enemy.Kill();
         }
     }
---
+++ Assets/MurfioBros/Scripts/Player.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Collider2D))]
+public class Player : MonoBehaviour {
+    private bool m_isInvulnerable = false;
+    private float m_invulnerabiltyTime = 1f;
+    private int m_health = 3;
+    private Color m_flashColor;
+    private float m_flashDuration = 0.25f;
+
+    private SpriteRenderer m_renderer;
+
+    private void Start() {
+        m_renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update() {
+        if (m_isInvulnerable) {
+            Flash();
+        }
+    }
+
+    private void Flash() {
+        m_flashColor = m_renderer.color;
+        m_flashColor.a = Mathf.Lerp(0, 1f, Mathf.PingPong(Time.time, m_flashDuration) / m_flashDuration);
+        m_renderer.color = m_flashColor;    
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.transform.tag == "Enemy" && 
+            collision.collider is BoxCollider2D && 
+            collision.contacts[0].otherCollider is BoxCollider2D) {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage() {
+        if (m_isInvulnerable) return;
+
+        Debug.Log("HIT!");
+
+        m_health--;
+
+        if (m_health <= 0) {
+            Die();
+        }
+        else {
+            StartCoroutine(PostHitInvulnerablity());
+        }
+    }
+
+    private void Die() {
+        Debug.Log("DEAD");
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator PostHitInvulnerablity() {
+        m_isInvulnerable = true;
+        yield return new WaitForSeconds(m_invulnerabiltyTime);
+        m_isInvulnerable = false;
+
+        m_flashColor.a = 1f;
+        m_renderer.color = m_flashColor;
+    }
+}
